94f6176ca34c9040965cbb0cd6b2f9e5822c2afd
Shanmathi Babu <myshanmathi864@gmail.com>
2024-07-31 13:39:27 +0530

    Mycommit-6

---
+++ Angular/MyAngularProject1/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { RightTopComponent } from './right-top/right-top.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    RightTopComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [
+    provideClientHydration()
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
